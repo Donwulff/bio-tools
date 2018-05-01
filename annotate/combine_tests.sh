9028e711decbcc01056a17fe25e995e8f414bbd7
donwulff <donwulff@nic.fi>
2018-05-02 00:12:15 +0300

    Drop temporary files into separate directory to reduce clutter.

--- annotate/combine_tests.sh
+++ annotate/combine_tests.sh
@@ -5,6 +5,9 @@ TSV=genome_v3_Full
 GFG=GFG_annotated_unphased_genotypes
 
 HS37D5=/mnt/GenomicData/hs37d5/hs37d5.fa
+WORK=work
+
+mkdir -p ${WORK}
 
 convert_23andMe()
 {
@@ -32,10 +35,10 @@ convert_23andMe
 convert_genes4good
 
 # Put 23andMe conversion first, since it has correct contigs; delete duplicates.
-bcftools concat -a -D ${TSV}.norm.vcf.gz ${GFG}.norm.vcf.gz | bgzip -c > merged.vcf.gz
+bcftools concat -a -D ${TSV}.norm.vcf.gz ${GFG}.norm.vcf.gz | bgzip -c > ${WORK}/merged.vcf.gz
 
 # NB. if tested variants are different and thus the results differ, they're removed.
 # These should probably be accepted for the the different alt, but it's tricky (What if both variants are 1/0?)
-vcftools --gzvcf ${TSV}.norm.vcf.gz --gzdiff ${GFG}.norm.vcf.gz --diff-site-discordance --out merged
-grep -v -- "-nan" merged.diff.sites  | grep -v "0$" > discordant.sites
-vcftools --gzvcf merged.vcf.gz --exclude-positions discordant.sites --out filtered --recode --stdout | bgzip -c > filtered.vcf.gz
+vcftools --gzvcf ${TSV}.norm.vcf.gz --gzdiff ${GFG}.norm.vcf.gz --diff-site-discordance --out ${WORK}/merged
+grep -v -- "-nan" ${WORK}/merged.diff.sites  | grep -v "0$" > ${WORK}/discordant.sites
+vcftools --gzvcf ${WORK}/merged.vcf.gz --exclude-positions ${WORK}/discordant.sites --out ${WORK}/filtered --recode --stdout | bgzip -c > filtered.vcf.gz
