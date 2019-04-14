e2a57260909b8965250e6bcaab60749eeee73f39
donwulff <donwulff@nic.fi>
2019-04-14 18:08:42 +0000

    Great renaming to clean up variable names and make bio-tools.cfg more compatible between scripts

--- mapping/revert-bam.sh
+++ mapping/revert-bam.sh
@@ -11,14 +11,14 @@
 # This script preserves it in unmapped bam and filters it out before processing, but the extra space and Casava header can be cleaned up with:
 # (samtools view -H sample1.bam; samtools view sample1.bam | awk '{ OFS="\t"; split($1,q," "); $1=q[1]; print }') | samtools view -b -o sample1.cleaned.bam
 
-# Different fs from data files, prefer SSD
-tmp=${TMP:-"/tmp"}
-
 # File locations
-bamfile=${1:-sample1.bam}
-outfile=${bamfile%%.bam}.unmapped.bam
-reference=hs38DH.fa
-compress=5
+SAMPLE=${1:-sample1.bam}
+BASENAME=${SAMPLE%%.bam}
+UBAMFILE=${BASENAME}.unmapped.bam
+BMAPFILE=${BASENAME}.bwamem.bam
+SORTFILE=${BASENAME}.sorted.bam
+REF=hs38DH.fa
+COMPRESS=5
 
 # Extra options for BWA, ie. long reads with "-x pacbio" etc.
 BWAOPT=
@@ -33,15 +33,27 @@ SANITIZE=true
 # Reference: https://www.ncbi.nlm.nih.gov/pubmed/30423086
 if which fastp > /dev/null;
 then
-  BASENAME=${bamfile%%.bam}
   FILTER="| fastp -QLGp --stdin --stdout --interleaved_in -j ${BASENAME}.fastp.json -h ${BASENAME}.fastp.html -R \"fastp report on ${BASENAME}\""
 fi
+
+# Cores and memory
+totalmem=`LC_ALL=C free | grep -e "^Mem:" | gawk '{print $7}'`
+# Allow 2 gigabytes for runtime
+javamem=${2:-$((totalmem/1024/1024-2))}
+# https://sourceforge.net/p/picard/wiki/Main_Page/#q-a-picard-program-that-sorts-its-output-sambam-file-is-taking-a-very-long-time-andor-running-out-of-memory-what-can-i-do
+bamrecords=$((javamem*250000))
+cores=`nproc`
+# From Java 6 update 18 max. heap is 1/4th of physical memory, so we can split 3/4th between cores for sorting.
+percoremem=$((javamem*3/4/cores))
 
 # If bio-tools.cfg exists where the script is run from, over-ride settings from it.
 if [ -e ./bio-tools.cfg ];
 then
   . ./bio-tools.cfg
 fi
+
+# Different fs from data files, prefer SSD
+tmp=${TMP:-"/tmp"}
 
 ## Combining BigY hs37 and GRCh38, I removed reference and SANITIZE=true and ran:
 # TMP=/mnt/SSD/ ./revert-bam.sh sample1BigY.bam
@@ -50,10 +62,10 @@ fi
 # samtools merge -@4 -c -f sample1BigYmerge.bam sample1BigY.unmapped.bam sample1BigY2.unmapped.bam
 ## RevertSam expects reads in specific order, Picard Tools queryname order works.
 # java -Xmx30G -jar picard.jar SortSam I=sample1BigYmerge.bam O=sample1BigYmerge.qsort.bam SORT_ORDER=queryname
-## Added reference but not SANITIZE, bwa undocumented removed duplicate read-id's:
+## Added reference but not SANITIZE that would remove all duplicates, bwa undocumented removed duplicate read-id's:
 # TMP=/mnt/SSD/ ./revert-bam.sh sample1BigYmerge.qsort.bam
 
-if [ ! -e $bamfile ];
+if [ ! -e ${SAMPLE} ];
 then
   echo "Runs Picard Tools on Java to create Broad Institute uBAM from input BAM"
   echo "$0 [input.bam] [gigabytes of memory to use, if not all available memory]"
@@ -69,20 +81,20 @@ fi
 check_space ()
 {
   # Figure out sizes in bytes
-  inputsize=`du -D -b $bamfile | gawk '{print $1}'`
-  outfree=`LC_ALL=C df -B1 $bamfile | grep -v "^File" | gawk '{print $4}'`
+  inputsize=`du -D -b ${SAMPLE} | gawk '{print $1}'`
+  outfree=`LC_ALL=C df -B1 ${SAMPLE%/*} | grep -v "^File" | gawk '{print $4}'`
 
-  if [ $inputsize -gt $outfree ];
+  if [ ${inputsize} -gt ${outfree} ];
   then
-    echo "Output file $1 probably won't fit on remaining space"
+    echo "Output file $1 (${inputsize}) probably won't fit on remaining space (${outfree})"
     exit
   fi
 
   # Figure out sizes in bytes - rule of thumb temporary is usually 1.5x times input
   tempsize=$((inputsize+(inputsize/2)))
-  tempfree=`LC_ALL=C df -B1 $tmp | grep -v "^File" | gawk '{print $4}'`
+  tempfree=`LC_ALL=C df -B1 ${tmp} | grep -v "^File" | gawk '{print $4}'`
 
-  if [ $tempsize -gt $tempfree ];
+  if [ ${tempsize} -gt ${tempfree} ];
   then
     echo "Approximately 1.5X input size is required for temporary storage $tmp"
     echo "Run with TMP=/path/to/tmp to use different path"
@@ -90,24 +102,14 @@ check_space ()
   fi
 }
 
-# Cores and memory
-totalmem=`LC_ALL=C free | grep -e "^Mem:" | gawk '{print $7}'`
-# Allow 2 gigabytes for runtime
-javamem=${2:-$((totalmem/1024/1024-2))}
-# https://sourceforge.net/p/picard/wiki/Main_Page/#q-a-picard-program-that-sorts-its-output-sambam-file-is-taking-a-very-long-time-andor-running-out-of-memory-what-can-i-do
-bamrecords=$((javamem*250000))
-cores=`nproc`
-# From Java 6 update 18 max. heap is 1/4th of physical memory, so we can split 3/4th between cores for sorting.
-percoremem=$((javamem*3/4/cores))
-
-if [ ! -e $outfile ];
+if [ ! -e ${UBAMFILE} ];
 then
-  check_space $outfile
+  check_space ${UBAMFILE}
 
-  echo "Reverting $bamfile into $outfile with Picard Tools"
+  echo "Reverting $bamfile into ${UBAMFILE} with Picard Tools"
   java -Xmx${javamem}G -jar picard.jar RevertSam \
-    I=$bamfile \
-    O=$outfile \
+    I=${SAMPLE} \
+    O=${UBAMFILE} \
     SANITIZE=${SANITIZE} \
     MAX_DISCARD_FRACTION=0.005 \
     ATTRIBUTE_TO_CLEAR=XT \
@@ -122,55 +124,55 @@ then
     REMOVE_DUPLICATE_INFORMATION=true \
     REMOVE_ALIGNMENT_INFORMATION=true \
     MAX_RECORDS_IN_RAM=$bamrecords \
-    COMPRESSION_LEVEL=$compress \
+    COMPRESSION_LEVEL=${COMPRESS} \
     TMP_DIR=$tmp
 fi
 
 # WARNING: This is NOT according to the Broad Institute GATK 4.0 Best Practices, but leaving it here for reference
 # Does not perform quality control, adapter trimming or base quality score recalibration
-if [ -e $reference ];
+if [ -e ${REF} ];
 then
-  echo "Mapping, marking duplicates and chromosome order sorting $outfile into ${bamfile%%.bam}.srt.bam"
+  echo "Mapping, marking duplicates and chromosome order sorting ${UBAMFILE} into ${SORTFILE}"
 
   # Identify Complete Genomics style read names for library complexity estimation
   try_regex='CL10.......L.C([0-9]+)R([0-9]+)_([0-9]+)'
-  if samtools view $outfile | head -1 | cut -f1 | grep -q -E "$try_regex";
+  if samtools view ${UBAMFILE} | head -1 | cut -f1 | grep -q -E "${try_regex}";
   then
-    regex="READ_NAME_REGEX=$try_regex"
+    regex="READ_NAME_REGEX=${try_regex}"
   fi
 
   # Uses .hdr file also as a flag of whether mapping finished, in case we restart
-  if [ ! -e ${bamfile%%.bam}.mem.bam ] && [ ! -e ${outfile}.hdr ];
+  if [ ! -e ${BMAPFILE} ] && [ ! -e ${UBAMFILE}.hdr ];
   then
-    check_space ${bamfile%%.bam}.mem.bam
+    check_space ${BMAPFILE}
 
-    samtools view -H $outfile > ${outfile}.hdr
+    samtools view -H ${UBAMFILE} > ${UBAMFILE}.hdr
     # FTDNA BigY doesn't have PL tag, so add it for downstream processing.
-    if ! grep -q "^@RG.*PL:" ${outfile}.hdr;
+    if ! grep -q "^@RG.*PL:" ${UBAMFILE}.hdr;
     then
-      sed -i "s/^@RG\t.*/&\tPL:ILLUMINA/" ${outfile}.hdr
+      sed -i "s/^@RG\t.*/&\tPL:ILLUMINA/" ${UBAMFILE}.hdr
     fi
 
     # Casava 1.8 header, observed s/[12]:[YN]:[0-9]*:[^\/]*\/[12]\t//
     # According to Wikipedia this can also end in barcode or sample-ID, so removing any.
-    samtools fastq -t $outfile \
+    samtools fastq -t ${UBAMFILE} \
       | eval sed "s/[12]:[YN]:[0-9]*:[^[:space:]]*[[:space:]]//" \
-      $FILTER \
-      | bwa mem $BWAOPT -p -t $cores -M -C -H ${outfile}.hdr $reference - \
-      | samtools view -b -o ${bamfile%%.bam}.mem.bam
-    rm ${outfile}.hdr
+      ${FILTER} \
+      | bwa mem ${BWAOPT} -p -t ${cores} -M -C -H ${UBAMFILE}.hdr ${REF} - \
+      | samtools view -b -o ${BMAPFILE}
+    rm ${UBAMFILE}.hdr
   fi
 
-  check_space ${bamfile%%.bam}.srt.bam
+  check_space ${SORTFILE}
 
   # Unfortunately, MarkDuplicates seeks back to beginning of the input BAM so mapping can't just be piped in
-  java -jar picard.jar MarkDuplicates INPUT=${bamfile%%.bam}.mem.bam OUTPUT=/dev/stdout METRICS_FILE=${bamfile}.dup \
+  java -jar picard.jar MarkDuplicates INPUT=${BMAPFILE} OUTPUT=/dev/stdout METRICS_FILE=${SAMPLE}.dup \
     ASSUME_SORT_ORDER=queryname TAGGING_POLICY=All COMPRESSION_LEVEL=0 TMP_DIR=$tmp \
-    OPTICAL_DUPLICATE_PIXEL_DISTANCE=2500 $regex \
-      | samtools sort -T $tmp/${bamfile##*/} -@$cores -m${percoremem}G -l${compress} -o ${bamfile%%.bam}.srt.bam
+    OPTICAL_DUPLICATE_PIXEL_DISTANCE=2500 ${regex} \
+      | samtools sort -T $tmp/${SAMPLE##*/} -@${cores} -m${percoremem}G -l${COMPRESS} -o ${SORTFILE}
   # set -e and set -o pipefail require bash, so check the final file is intact instead
-  if samtools quickcheck ${bamfile%%.bam}.srt.bam;
+  if samtools quickcheck ${SORTFILE};
   then
-    rm ${bamfile%%.bam}.mem.bam
+    rm ${BMAPFILE}
   fi
 fi
