3e33dd4f4a755857ab7c8291606328f048dc78d9
donwulff <donwulff@nic.fi>
2019-09-14 14:32:02 +0000

    Filter out alt-rows without perfect mapping quality

--- mapping/GRCh38_bwa_index.sh
+++ mapping/GRCh38_bwa_index.sh
@@ -116,7 +116,7 @@ fi
 # There's no documentation for how the bwa alt-file should be constructed. This is just a basic starting point.
 cat hg38Patch11.fa.gz GRCh38Patch12.fa.gz GRCh38Patch13.fa.gz hla_gen.fasta.gz > additional_hg38_contigs.fa.gz
 bwa mem -t`nproc` -x intractg GCA_000001405.15_GRCh38_no_alt_analysis_set.fna additional_hg38_contigs.fa.gz \
-  | samtools view - \
+  | samtools view -q60 - \
   | gawk '{ OFS="\t"; $10 = "*"; print }' > additional_hg38_contigs.map
 
 zcat GCA_000001405.15_GRCh38_full_analysis_set.fna.gz GCA_000786075.2_hs38d1_genomic_unmapped.fna.gz \
